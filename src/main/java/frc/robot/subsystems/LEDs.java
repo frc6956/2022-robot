358fea3791bcbba028d2490c7db8d5581b888cbc
SHAM-ROCK-BOTICS <shamrockbotics@gmail.com>
2022-03-10 14:19:49 -0500

    Added VisionOff, updated LEDs, changed constants

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -11,9 +11,9 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 public class LEDs extends SubsystemBase {
 
   private AddressableLED m_led;
- // private AddressableLED m_led2;
- // private AddressableLED m_led3;
+
   private AddressableLEDBuffer m_ledBuffer;
+
 
 
   /** Creates a new LEDs. */
@@ -21,11 +21,11 @@ public class LEDs extends SubsystemBase {
     // PWM port 9
     // Must be a PWM header, not MXP or DIO
     m_led = new AddressableLED(9);
-   // m_led2 = new AddressableLED(8);
-   // m_led3 = new AddressableLED(7);
+
     // Reuse buffer
     // Default to a length of 60, start empty output // 47
     m_ledBuffer = new AddressableLEDBuffer(47);
+
 
     setUpLight();
   }
@@ -33,47 +33,55 @@ public class LEDs extends SubsystemBase {
   public void setUpLight() { // gets LED segment length and sets the data for them
     // Length is expensive to set, so only set it once, then just update data
     m_led.setLength(m_ledBuffer.getLength());
-   // m_led2.setLength(m_ledBuffer.getLength());
-   // m_led3.setLength(m_ledBuffer.getLength());
 
     // Set the data
     m_led.setData(m_ledBuffer);
     m_led.start();
 
-   // m_led2.setData(m_ledBuffer);
-   // m_led2.start();
-
-   // m_led3.setData(m_ledBuffer);
-  //  m_led3.start();
   }
 
   public void setAllGreen(){ // sets all LEDs to green
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       // Sets the specified LED to the RGB values for red
-      m_ledBuffer.setRGB(i, 0, 0, 255);
+      m_ledBuffer.setRGB(i, 0, 255, 0);
    }
    
    m_led.setData(m_ledBuffer);
-   //m_led2.setData(m_ledBuffer);
-   //m_led3.setData(m_ledBuffer);
   }
 
-  
-  public void setRedToGreen(){ // runs LEDs that change colors from red to green 
+   public void setYellowToGreen(){ // runs LEDs that change colors from red to green 
     double m_rainbowFirstPixelHue = 0;
       // For every pixel
       for (var i = 0; i < m_ledBuffer.getLength(); i++) {
         // Calculate the hue - hue is easier for rainbows because the color
         // shape is a circle so only one value needs to precess
-        final int hue = (int)(m_rainbowFirstPixelHue + (i * 60 / m_ledBuffer.getLength())) % 60; // hue is red to green
+        final int hue = ((int)(m_rainbowFirstPixelHue + (i * 55 / m_ledBuffer.getLength())) % 55) + 20; // hue is red to green
         // Set the value
         m_ledBuffer.setHSV(i, hue, 255, 128);
+      
       }
       // Increase by to make the rainbow "move"
-      m_rainbowFirstPixelHue += 3;
+      m_rainbowFirstPixelHue += 2;
       // Check bounds
-      m_rainbowFirstPixelHue %= 60;
+      m_rainbowFirstPixelHue %= 55;
   }
+
+  public void rainbow() {
+    double m_rainbowFirstPixelHue = 0;
+    // For every pixel
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = ((int)(m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180);
+      // Set the value
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    m_rainbowFirstPixelHue += 3;
+    // Check bounds
+    m_rainbowFirstPixelHue %= 180;
+  }
+
 
   public void shooterColorSpeed(double rpm){ // runs LEDs in which the shooter rpm controls how fast it changes color
     double m_firstPixelHue = 0; // sets the beginning hue to red
@@ -87,30 +95,34 @@ public class LEDs extends SubsystemBase {
   hueSpeed = ((rpm - shooterMinRPM)/(shooterMaxRPM - shooterMinRPM))*(hueSpeedMax - hueSpeedMin) + hueSpeedMin;
 
     // changes starting hue based on current speed
-    if (rpm < 1000){
+    if (rpm > 0){
       m_firstPixelHue = 0; // red
     }
-    else if (rpm < 3000){
+    else if (rpm > 500){
       m_firstPixelHue = 20; // red orange
     }
-    else if (rpm < 5000){
+    else if (rpm > 1000){
       m_firstPixelHue = 25; // orange
     }
-    else{
+    else if (rpm > 1500){
       m_firstPixelHue = 35; // yellow
+    }
+    else {
+      m_firstPixelHue = 48; // green
     }
     // For every pixel
     for (var i = 0; i < m_ledBuffer.getLength(); i++) {
       // Calculate the hue - hue is easier for rainbows because the color
       // shape is a circle so only one value needs to precess
-      final int hue = (int)(m_firstPixelHue + (i * 60 / m_ledBuffer.getLength())) % 60; // hue is red to green
+      final int hue = (int)(m_firstPixelHue + (i * 65 / m_ledBuffer.getLength())) % 65; // hue is red to green
       // Set the value
       m_ledBuffer.setHSV(i, hue, 255, 128);
+
     }
     // Increase by to make the rainbow "move"
     m_firstPixelHue += hueSpeed; //the hue changes color slower or quicker depending on the rpm of the shooter
     // Check bounds
-    m_firstPixelHue %= 60;
+    m_firstPixelHue %= 71;
   }
 
   
@@ -118,10 +130,9 @@ public class LEDs extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    setRedToGreen();
+    //setRedToGreen();
     // Set the LEDs
     m_led.setData(m_ledBuffer);
-    //m_led2.setData(m_ledBuffer);
-    //m_led3.setData(m_ledBuffer);
+  
   }
 }
